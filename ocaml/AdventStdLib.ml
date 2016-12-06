343cfb85205136d78567f08d0f92d791a77a6e33
Spencer Williams <spencerwi@gmail.com>
2016-12-06 09:43:45 -0500

    I misunderstood day1's directions; 'visited' includes 'along the way', not just terminal intersections

--- ocaml/AdventStdLib.ml
+++ ocaml/AdventStdLib.ml
@@ -9,3 +9,14 @@ let read_lines_from_file (filename: string) : string list =
     with End_of_file -> 
         close_in chan;
         List.rev !lines
+
+let rec int_range (start: int) (stop: int) ?step:(s=1) : int list =
+    if start = stop 
+    then []
+    else 
+        match (s > 0, start > stop) with
+        | true, true  -> []
+        | true, false -> start :: (int_range (start + s) stop ~step:s)
+        | false, true -> start :: (int_range (start + s) stop ~step:s)
+        | false, false -> []
+
--- ocaml/day1.ml
+++ ocaml/day1.ml
@@ -10,7 +10,6 @@ module Grid = struct
         | 'L' -> L
         | 'R' -> R
         | _   -> failwith "Bad direction!"
-
     let parse_move (s: string): move =
         let turn_direction: turn_direction = (String.get s 0 |> parse_turn_direction) in
         let steps: int = (String.sub s 1 ((String.length s) - 1) |> int_of_string) in
@@ -36,16 +35,60 @@ module Grid = struct
     let is_same_location (state1: state) (state2: state) : bool =
         (state1.x = state2.x) && (state1.y = state2.y)
 
+    let states_between (state1: state) (state2: state) : state list =
+        match state2.facing with
+        | N | S -> 
+            begin
+                let step = 
+                    if state1.y > state2.y
+                    then -1
+                    else 1
+                in
+                AdventStdLib.int_range (state1.y + step) state2.y ~step |> List.map (fun y -> {facing = state2.facing; x = state2.x; y})
+            end
+        | E | W ->
+            begin
+                let step = 
+                    if state1.x > state2.x
+                    then -1
+                    else 1
+                in
+                AdventStdLib.int_range (state1.x + step) state2.x ~step |> List.map (fun x -> {facing = state2.facing; y = state2.y; x})
+            end
+            
+
     let blocks_between (state1: state) (state2: state) : int =
         (abs (state2.x - state1.x)) + (abs (state2.y - state1.y))
 
-    let string_of_state ({x;y}) = Printf.sprintf "{x: %d, y: %d}" x y
+    let string_of_state ({facing;x;y}) = 
+        let facing_str = match facing with
+            | N -> "N"
+            | S -> "S"
+            | E -> "E"
+            | W -> "W"
+        in
+        Printf.sprintf "%s(%d,%d)" facing_str x y
+    let string_of_move ({turn;steps}) =
+        let turn_str = match turn with
+            | L -> "L"
+            | R -> "R"
+        in
+        Printf.sprintf "%s%d" turn_str steps
+
+    module StateSet = Set.Make(struct
+        type t = state
+        let compare s1 s2 = 
+            if s1.x = s2.x && s1.y = s2.y 
+            then 0
+            else compare (string_of_state s1) (string_of_state s2)
+    end)
 end
 
 
 let read_day1 () : Grid.move list =
     AdventStdLib.read_lines_from_file "../inputs/day1.txt"
     |> (fun l -> List.nth l 0)
+    (* "R8, R4, R4, R8" *)
     |> Str.split (Str.regexp ", ")
     |> List.map Grid.parse_move
 
@@ -58,20 +101,27 @@ let day1A () =
 
 let day1B () =
     let moves = read_day1() in
-    let rec step (seen_states: Grid.state list) (current_state: Grid.state) = function
+    let rec find_twice_visited_spot (seen_states: Grid.StateSet.t) (current_state: Grid.state) = function
         | [] -> None
         | next_move::rest -> 
             let new_state = Grid.apply_move current_state next_move in
-            let has_seen_state = List.exists (Grid.is_same_location new_state) seen_states in
-            (* Printf.printf "current_state: %s\n" (Grid.string_of_state current_state); *)
-            (* Printf.printf "new_state: %s\n" (Grid.string_of_state new_state); *)
-            (* Printf.printf "seen_state: %s\n" (List.map Grid.string_of_state seen_states |> String.concat "\n\t "); *)
-            (* Printf.printf "has_seen_state: %b\n" has_seen_state; *)
-            if has_seen_state
-            then Some new_state
-            else step (new_state :: seen_states) new_state rest
+            let states_traversed = Grid.states_between current_state new_state in
+            (* Printf.printf "%s-->" (Grid.string_of_state current_state); *)
+            (* List.map Grid.string_of_state states_traversed |> String.concat "-->" |> print_string; *)
+            (* Printf.printf "-->%s" (Grid.string_of_state new_state); *)
+            let has_seen_spot = List.exists (fun state -> Grid.StateSet.mem state seen_states) states_traversed in
+            if has_seen_spot
+            then begin
+                (* print_endline ""; *)
+                let state_already_seen = List.find (fun state -> Grid.StateSet.mem state seen_states) states_traversed in
+                Some state_already_seen
+            end
+            else begin
+                (* print_string "-->"; *)
+                find_twice_visited_spot (Grid.StateSet.union seen_states (Grid.StateSet.of_list (states_traversed @ [new_state]))) new_state rest
+            end
     in
-    step [Grid.initial_state] Grid.initial_state moves
+    find_twice_visited_spot (Grid.StateSet.singleton Grid.initial_state) Grid.initial_state moves
     |> (function 
             | None -> failwith "Something went wrong; we didn't visit any state twice"
             | Some final_state -> Grid.blocks_between Grid.initial_state final_state)
