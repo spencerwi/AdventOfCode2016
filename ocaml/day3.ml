3990f635b377b79016a267189deef18ffaf09e83
Spencer Williams <spencerwi@gmail.com>
2016-12-07 09:20:25 -0500

    Day3B solution in OCaml

--- ocaml/day3.ml
+++ ocaml/day3.ml
@@ -23,5 +23,20 @@ let day3a () : int =
     |> List.filter (fun (a,b,c) -> is_valid_triangle a b c)
     |> List.length
 
+let day3b () : int =
+    let rec read_columns_by_threes = function
+        | [] -> []
+        | one::two::three::rest ->
+            let (a1, b1, c1) = one in
+            let (a2, b2, c2) = two in
+            let (a3, b3, c3) = three in
+            [(a1, a2, a3); (b1, b2, b3); (c1, c2, c3)] @ (read_columns_by_threes rest)
+    in
+    read_day3()
+    |> read_columns_by_threes
+    |> List.filter (fun (a,b,c) -> is_valid_triangle a b c) 
+    |> List.length
+
 let () =
-    Printf.printf "Day 3A: %d\n" (day3a())
+    Printf.printf "Day 3A: %d\n" (day3a());
+    Printf.printf "Day 3B: %d\n" (day3b())
